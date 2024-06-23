3039359cdc6aee21d4adf29cabf52f3d40f4358c
Dmitri Snytkine <d.snytkine@gmail.com>
2024-06-23 09:22:13 -0400

    Added CustomAdminAuthenticator

--- src/main/java/com/snytkine/wiremock_handler/WiremockHandlerApplication.java
+++ src/main/java/com/snytkine/wiremock_handler/WiremockHandlerApplication.java
@@ -2,16 +2,38 @@ package com.snytkine.wiremock_handler;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.snytkine.wiremock_handler.plugin.CustomAdminAuthenticator;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j(topic = "WiremockHandlerApplication")
 public class WiremockHandlerApplication {
 
-	public static void main(String[] args) {
-		
-		WireMockServer wiremock = new WireMockServer(WireMockConfiguration.options().port(8898));
-		wiremock.start();
-		System.out.println("~~~~~~~~~~~~~ wiremock started on port " + wiremock.port());
+	private static final String DEFAULT_SERVER_PORT = "8898";
 
+	public static void main(String[] args) {
+
+		String adminUser = System.getenv().get("WIREMOCK_ADMIN_USER");
+		String adminPassword = System.getenv().get("WIREMOCK_ADMIN_PASSWORD");
+
+		if (adminUser == null || adminPassword == null) {
+			throw new RuntimeException(
+					"WIREMOCK_ADMIN_USER and WIREMOCK_ADMIN_PASSWORD environment variables must be set prior to starting this application");
+		}
+		String wiremockServerPort = System.getenv().get("WIREMOCK_SERVER_PORT");
+		if (wiremockServerPort == null) {
+			wiremockServerPort = DEFAULT_SERVER_PORT;
+		}
+		int serverPort = Integer.parseInt(wiremockServerPort);
+
+		var customAdminAuthenticator = new CustomAdminAuthenticator(adminUser, adminPassword);
+
+		WireMockServer wiremock = new WireMockServer(
+				WireMockConfiguration.options()
+						.port(serverPort)
+						.adminAuthenticator(customAdminAuthenticator));
+		wiremock.start();
+		log.info("WireMock started on port {}", wiremock.port());
 	}
 
 }
---
+++ src/main/java/com/snytkine/wiremock_handler/plugin/CustomAdminAuthenticator.java
@@ -0,0 +1,38 @@
+package com.snytkine.wiremock_handler.plugin;
+
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.security.Authenticator;
+import com.github.tomakehurst.wiremock.security.BasicAuthenticator;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "CustomAdminAuthenticator")
+public class CustomAdminAuthenticator implements Authenticator {
+
+    private BasicAuthenticator basicAuthenticator;
+
+    public CustomAdminAuthenticator(String username, String password) {
+        this.basicAuthenticator = new BasicAuthenticator(username, password);
+    }
+
+    @Override
+    public boolean authenticate(Request request) {
+
+        var url = request.getUrl();
+        var method = request.getMethod().toString();
+        log.info("ADMINURL={} method={}", url, method);
+
+        if (url.equals("/shutdown")) {
+            log.error("Not allowed to shutdown running server");
+            return false;
+        }
+
+        if (method.equals("GET")) {
+            log.info("GET request is allowed for __admin endpoint {}", url);
+            return true;
+        }
+
+        return basicAuthenticator.authenticate(request);
+    }
+
+}
